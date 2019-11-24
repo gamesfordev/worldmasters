6d67fc53dbf548be23dba79509029b89779d5fd1
athifs7 <athifs@99x.lk>
2019-11-24 21:03:25 +0530

    Experimental socket

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const  app = require('./app');
-const io = require('socket.io');
-
+// const io = require('socket.io');
+var http = require('http');
+var server = http.Server(app);
 const httpPort = process.env.PORT  ||5000;
 
-const socketPort = process.env.PORT|| 5001;
-const socketServer = io.listen(socketPort);
+// const socketPort = process.env.PORT|| 5001;
+// const socketServer = io.listen(socketPort);
 const gameEngine = require('./game-engine');
 
-app.listen(httpPort, function(){
+server.listen(httpPort, function(){
   console.log(`INFO: Listening to ${httpPort}`);
 });
 
-socketServer.on('connection', (socket) => {
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
 
     gameEngine.init(socketServer);
 
